da456b32d306a40af0265876e76dbe8e2002dc08
kenjpete <kjpetersen@gmail.com>
2015-11-19 06:16:07 -0500

    Initial build

---
+++ icc08-compass/scripts/scripts.js
@@ -0,0 +1,20 @@
+﻿var watchID;
+var CompassDiv = document.getElementById("DivHeading");
+
+
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    watchID = navigator.compass.watchHeading(onSuccess, onError, options);
+}
+
+function onSuccess(heading) {
+    CompassDiv.innerHTML = 'Heading: ' + heading.magneticHeading;
+};
+
+function onError(compassError) {
+    CompassDiv.innerHTML = 'Compass error: ' + compassError.code;
+};
+
+var options = {
+    frequency: 1000
+}; // Update every 1 second
